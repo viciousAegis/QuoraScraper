f8913327de23acc8bd88c9c1e1c4f4af17d20369
Akshit Sinha <akshitsinha3@gmail.com>
2024-02-02 01:15:51 +0530

    new

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
-import os
-import requests
+from flask_cors import CORS, cross_origin
+import subprocess
 
 app = Flask(__name__)
 
@@ -8,12 +8,27 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+def run_scraper(post_count):
+    # call scraper.py
+    subprocess.Popen(['python3', 'run.py', '-c', str(post_count)])
+
 @app.route('/scrape', methods=['POST'])
+@cross_origin(supports_credentials=True, origins='*')
 def scrape_data():
     # get params from request
-    search_terms = request.args.get('search_terms')
+    search_terms = request.json['search_terms']
+    try:
+        post_count = request.json['post_count']
+    except:
+        post_count = 10
 
     print(search_terms)
+    # populate search.terms file
+    with open('./search.terms', 'w') as f:
+        for search_term in search_terms:
+            f.write(search_term + '\n')
 
-    # call scraper.py
-    # return json
+    # run scraper.py
+    run_scraper(post_count)
+    
+    return 'running scraper.py'
